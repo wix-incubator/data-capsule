d88e632ffd272267d9f6bbdd9684d93035383b66
Shahar Talmi <shahart@wix.com>
2017-01-17 22:43:00 +0200

    added frame bundle

---
+++ src/frame.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const FrameStorageStrategy = require('./strategies/frame-storage');
+const {NOT_FOUND} = require('./utils/constants');
+const BaseStorage = require('./base-storage');
+const DataCapsule = require('./data-capsule');
+
+module.exports = {
+  NOT_FOUND,
+  BaseStorage,
+  DataCapsule,
+  FrameStorageStrategy
+};
