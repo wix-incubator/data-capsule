403e85150a63a9c3d67e4048bdf6f43b2d7902ea
Ran Yitzhaki <ranyitzhaki@gmail.com>
2017-08-13 18:41:02 +0300

    remove async-await from from frame-storage-listener

--- src/utils/frame-storage-listener.js
+++ src/utils/frame-storage-listener.js
@@ -16,7 +16,7 @@ class FrameStorageListener {
     //   return;
     // }
     const storageStrategy = BaseStorage.verify(this.storageStrategy);
-    this._listener = async e => {
+    this._listener = e => {
       const [target, token, id, method, params] = greedySplit(e.data, '|', 5);
       const respond = (method, param) => {
         const message = [target + 'Done', token, id, method, JSON.stringify(param)].join('|');
@@ -25,13 +25,15 @@ class FrameStorageListener {
 
       if (target === STORAGE_PREFIX && verifier(e.source, e.origin, token)) {
         const invoke = storageStrategy[method].bind(storageStrategy);
-        try {
-          respond('resolve', await invoke(...JSON.parse(params)));
-        } catch (reason) {
+        invoke(...JSON.parse(params))
+        .then(result => {
+          respond('resolve', result);
+        }).catch(reason => {
           respond('reject', reason.message || reason);
-        }
+        });
       }
     };
+
     window.addEventListener('message', this._listener);
   }
 
