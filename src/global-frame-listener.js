10409298d4ea9717008b899424187ee90d589f62
Ran Yitzhaki <ranyitzhaki@gmail.com>
2017-08-20 12:24:00 +0300

    only add global variable 'DataCapsuleTools' to window if window exists

--- src/global-frame-listener.js
+++ src/global-frame-listener.js
@@ -2,6 +2,8 @@
 'use strict';
 const dataCapsuleTools = require('./frame-listener');
 
-window.DataCapsuleTools = dataCapsuleTools;
+if (typeof window !== 'undefined') {
+  window.DataCapsuleTools = dataCapsuleTools;
+}
 
 module.exports = dataCapsuleTools;
