f8a3699970b86be9cd33cb2727ab35bbcb3b8eb1
builduser <buildserver@wix.com>
2020-07-25 03:21:23 +0000

    bower version 1.0.299

--- frame.bundle.js
+++ frame.bundle.js
@@ -87,7 +87,7 @@ return /******/ (function(modules) { // webpackBootstrap
 /******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
 /******/
 /******/ 	// __webpack_public_path__
-/******/ 	__webpack_require__.p = "https://static.parastorage.com/services/data-capsule/1.505.0/";
+/******/ 	__webpack_require__.p = "https://static.parastorage.com/services/data-capsule/1.506.0/";
 /******/
 /******/
 /******/ 	// Load entry module and return exports
